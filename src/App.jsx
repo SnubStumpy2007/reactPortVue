4af09e726ff4e579bc4f446d953cfe36396fc0cb
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-23 19:29:44 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 
 const App =() => {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    3d dev port
-   </div>
+    <BroswerRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover
+        bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BroswerRouter>
   )
 }
 
